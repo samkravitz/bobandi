7c63bf77923a02ab546f3409cad31931b9c730cc
Sam Kravitz <samkravitz99@gmail.com>
2022-01-17 12:31:17 -0500

    Hardcode a stream of one of the bot's test games

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const axios = require('axios')
+const hyperquest = require('hyperquest')
+const ndjson = require('ndjson')
 
 const headers = {
     Authorization: `Bearer ${process.env.lichessToken}`,
+    Accept: 'application/x-ndjson',
 }
 
+hyperquest('https://lichess.org/api/bot/game/stream/wfFigTQaXM9S', { headers })
+    .pipe(ndjson.parse())
+    .on('data', console.log)
 
-axios.get('https://lichess.org/api/account', { headers })
-    .then(res => console.log(res.data))
+//axios.get('https://lichess.org/api/account', { headers })
+//    .then(res => console.log(res.data))
